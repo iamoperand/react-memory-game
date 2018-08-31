f0d2209b76cce0f9da2d0b66b4159bc3dd32f178
Nikhil Arora <narora200@gmail.com>
2018-08-31 16:34:46 +0530

    [Feat]: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello React
+      </div>
+    );
+  }
+}
+
+export default App;
