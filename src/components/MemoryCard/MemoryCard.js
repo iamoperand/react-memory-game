fba2fe67b400264a0d4c07d8c7d2edccbd8a0d85
Nikhil Arora <narora200@gmail.com>
2018-09-02 08:29:05 +0530

    [Feat]: Images added to Playground

--- src/components/MemoryCard/MemoryCard.js
+++ src/components/MemoryCard/MemoryCard.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
-// import { Card } from 'antd';
+import {
+  Card,
+  Avatar,
+} from 'antd';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+
+const LargeAvatar = styled(Avatar)`
+  width: 100%;
+  height: 90%;
+  padding: 0px;
+  position: absolute;
+`;
+
+const FlexCenteredCard = styled(Card)`
+  .ant-card-body {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  position: relative;
+  width: 17em;
+  height: 17em;
+`;
 
 class MemoryCard extends Component {
   render() {
+    const { frontImage, backImage } = this.props;
     return (
-      <div>
-        Memory Card
-      </div>
+      <FlexCenteredCard style={{ padding: '1em' }}>
+        <LargeAvatar src={frontImage} />
+        <LargeAvatar src={backImage} />
+      </FlexCenteredCard>
     );
   }
 }
+
+MemoryCard.propTypes = {
+  frontImage: PropTypes.string.isRequired,
+  backImage: PropTypes.string.isRequired,
+};
+
 
 export default MemoryCard;
